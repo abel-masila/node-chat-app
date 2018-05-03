68f7fd4cdb722bd6102bdaf51050ccc0709ccdb1
masila <abelmasila@gmail.com>
2018-05-03 20:59:06 +0300

    Added socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
+
+
+const port=process.env.PORT || 3000;
+const publicPath=path.join(__dirname ,'../public');
 
 const app=express();
-const port=process.env.PORT || 3000;
+const server=http.createServer(app);
+const io=socketIO(server);
 
-const publicPath=path.join(__dirname ,'../public');
+
 app.use(express.static(publicPath));
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect',()=>{
+        console.log('Client disconnected from server');
+    });
+});
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 });
 
