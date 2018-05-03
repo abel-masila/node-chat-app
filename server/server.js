1c0b6fd8d8b8890e6b5128ab20b6f21f2e8d1e18
masila <abelmasila@gmail.com>
2018-05-03 22:21:59 +0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ const io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected');
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user Joined',
+        createdAt: new Date().getTime()
+    })
     socket.on('disconnect',()=>{
         console.log('Client disconnected from server');
     });
@@ -27,6 +37,11 @@ io.on('connection',(socket)=>{
             text:message.text,
             createdAt:new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createdAt:new Date().getTime()
+        // });
     });
     
 });
