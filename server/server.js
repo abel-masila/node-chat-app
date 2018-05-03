5b673e859d278c009208aacef5ea6a63d17bdb1f
masila <abelmasila@gmail.com>
2018-05-03 20:18:18 +0300

    setup app for heroku deployment

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const path=require('path');
 const express=require('express');
 
 const app=express();
+const port=process.env.PORT || 3000;
 
 const publicPath=path.join(__dirname ,'../public');
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-    console.log(`Server is up on port 3000`);
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
 });
 
